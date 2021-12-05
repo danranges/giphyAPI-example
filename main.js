370036e82d387e710a2af783858e9ea0eb635000
Dan Ranges <danranges@gmail.com>
2021-12-05 17:48:06 +0100

    Add search and refresh functions

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 const img = document.querySelector('img');
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=W4zXlbYqgvo856mglQF739zN1e9U2uo1&s=cats', {
-  mode: 'cors',
-})
-  .then((response) => response.json())
-  .then((response) => (img.src = response.data.images.original.url));
+const btn = document.querySelector('button');
+
+const getNewGIFs = (query) =>
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=W4zXlbYqgvo856mglQF739zN1e9U2uo1&s=${query}`,
+    {
+      mode: 'cors',
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => (img.src = response.data.images.original.url));
+
+const searchTerm = () => {
+  const query = document.querySelector('input').value || 'cats';
+  return encodeURIComponent(query);
+};
+
+window.addEventListener('load', () => getNewGIFs('cats'));
+
+btn.addEventListener('click', () => getNewGIFs(searchTerm()));
